88d17e8498f9dbf917c7e8ef62ac72bb266153e2
Riccardo Zuliani <zuliani.riccardo.98@gmail.com>
2026-03-18 22:12:28 -0700

    updated todos

--- TODOs.py
+++ TODOs.py
@@ -3,3 +3,6 @@
 #TODO: outsource linear system solver in sparse mode
 #TODO: update dense docstrings
 #TODO: sparse mode has no warmstart
+#TODO: we should allow the solver to cache e.g. factorizations
+#TODO: we should also allow sparse linear solvers in dense mode
+# so that we can use fast linear solvers with dense QP solvers.
