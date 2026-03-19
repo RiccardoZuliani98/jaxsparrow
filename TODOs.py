c70b138e2919038414964f0764c0ba2c56524afe
Riccardo Zuliani <zuliani.riccardo.98@gmail.com>
2026-03-19 10:20:12 -0700

    updated todos

--- TODOs.py
+++ TODOs.py
@@ -10,3 +10,6 @@
 #TODO: add linear solver qdldl and better linear solvers in sparse mode,
 # also we should allow the solver to cache e.g. factorizations
 #TODO: add envelope theorem
+#TODO: we should exploit available information as much as possible
+# when building the solver, for example with PIQP we can use problem
+# setup followed by problem update.
