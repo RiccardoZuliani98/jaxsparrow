9cd93158ceb210de3a1b6b58feaca9be7cc1f61e
Riccardo Zuliani <zuliani.riccardo.98@gmail.com>
2026-03-18 18:15:25 -0700

    added todos

---
+++ TODOs.py
@@ -0,0 +1,7 @@
+#TODO: check timing printer
+#TODO: check finite difference checker
+#TODO: check sparse solver
+#TODO: verify validity of sparse tests
+#TODO: outsource linear system solver in sparse mode
+#TODO: some tests in sparse mode are failing
+#TODO: update all docstrings
