215739ab3b82091ed7c0806752617f9fb3abe4b1
Riccardo Zuliani <zuliani.riccardo.98@gmail.com>
2026-03-20 00:31:45 -0700

    testing out some solvers

--- TODOs.py
+++ TODOs.py
@@ -2,9 +2,6 @@
 #TODO: add diff-by-design algorithm
 #TODO: add bpmpc
 #TODO: add conic programming
-#TODO: add linear solver qdldl and better linear solvers in sparse mode,
-# also we should allow the solver to cache e.g. factorizations
+#TODO: add linear solver qdldl and allow the solver 
+# to cache e.g. factorizations
 #TODO: add envelope theorem
-#TODO: we should exploit available information as much as possible
-# when building the solver, for example with PIQP we can use problem
-# setup followed by problem update.
